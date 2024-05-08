c1f03dea95f360712817e0c02c5d2a58bdda1f7b
Mel <mel@Mels-MacBook-Pro.local>
2024-05-08 17:21:07 -0600

    feat:us1 complete

--- spec/features/users/discover/index_spec.rb
+++ spec/features/users/discover/index_spec.rb
@@ -6,33 +6,46 @@ RSpec.describe "Discover Index Page" do
   end
   
   # user story 1
-    it 'has a button to discover top rated movies' do
-      visit user_discover_index_path(@user1)
-
-      expect(page).to have_button('Discover Top Rated Movies')
-    end
-
-    it 'has a form to search by movie title' do
-      visit user_discover_index_path(@user1)
-
-      expect(page).to have_field('Enter a Movie Title')
-    end
-
-    it 'has a button to submit a search' do
+  it 'has a button to discover top rated movies' do
     visit user_discover_index_path(@user1)
 
-      within "#search_by_title" do
-        expect(page).to have_button('Search')
-      end
+    expect(page).to have_link('Discover Top Rated Movies')
+  end
+
+  it 'has a form to search by movie title' do
+    visit user_discover_index_path(@user1)
+
+    expect(page).to have_field('Enter a Movie Title')
+  end
+
+  it 'has a button to submit a search' do
+    visit user_discover_index_path(@user1)
+
+    within "#search_by_title" do
+      expect(page).to have_button('Search')
     end
+  end
 
   # user story 2
-    xit 'has a button that will redirect to the movies results page' do
+  it 'has a button that will redirect to the movies results page with the top 20 movies' do
+    VCR.use_cassette("top_rated_movies") do      
       visit user_discover_index_path(@user1)
 
       click_on 'Discover Top Rated Movies'
 
-      expect(current_path.to eq(user_movies_path(@user1)))
-      # expect(page).to have_content
+      expect(current_path).to eq(user_movies_path(@user1))
     end
   end
+
+  it 'has a button that will redirect to the movies results page with the search results' do
+    VCR.use_cassette("search_pulp_fiction") do
+      visit user_discover_index_path(@user1)
+
+      fill_in 'Enter a Movie Title', with: 'Pulp Fiction'
+      click_button 'Search'
+      
+      expect(current_path).to eq(user_movies_path(@user1))
+      expect(page).to have_content("Pulp Fiction")
+    end
+  end
+end
---
+++ spec/features/users/movies/index_spec.rb
@@ -0,0 +1,3 @@
+# test for in US 2: 
+# test for top 20 popular movies somehow
+# test for search results to appear
