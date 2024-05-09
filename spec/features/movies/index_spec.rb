5459f03daff3830b208225bd2dde07a91396d53c
Mel <mel@Mels-MacBook-Pro.local>
2024-05-08 20:20:06 -0600

    test:all berfa,berp,bers pass

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Movie Results Page" do
   before :each do
     @user1 = User.create!(name: "Joe", email: "jaja@blah.com")
+    @facade = MovieFacade.new("Pulp Fiction")
   end
 
   # user story 2
@@ -13,7 +14,7 @@ RSpec.describe "Movie Results Page" do
       click_on 'Discover Top Rated Movies'
 
       within "#movie_data" do
-        expect(page).to have_content("")
+        expect(page).to_not have_content("21")
       end
     end
   end
@@ -21,26 +22,26 @@ RSpec.describe "Movie Results Page" do
   it 'can show the search results of a user limited by 20' do
     VCR.use_cassette("twenty_top_search_results") do
       visit user_discover_index_path(@user1)
-      movie = "Pulp Fiction"
 
-      fill_in 'Enter a Movie Title', with: movie
+      fill_in 'Enter a Movie Title', with: "Pu"
       click_button 'Search'
 
       within "#movie_data" do
-        expect(page).to have_content(movie.vote_average)
+        expect(page).to_not have_content("21")
       end
     end
   end
 
   it 'has the movie title and link to the Movie Deets Page' do
     VCR.use_cassette("title_as_link") do
+      movie = "Pulp Fiction"
       visit user_discover_index_path(@user1)
 
-      fill_in 'Enter a Movie Title', with: 'Pulp Fiction'
+      fill_in 'Enter a Movie Title', with: movie
       click_button 'Search'
 
       within "#movie_data" do
-        expect(page).to have_content("")
+        expect(page).to have_link('Pulp Fiction', href: user_movie_path(@user1, @facade.movies.first.id))
       end
     end
   end
@@ -53,7 +54,7 @@ RSpec.describe "Movie Results Page" do
       click_button 'Search'
 
       within "#movie_data" do
-        expect(page).to have_content("")
+        expect(page).to have_content(@facade.movies.first.vote_average)
       end
     end
   end
