bde57564a02ddc9c5e5e827c21450248f9e45356
Mel <mel@Mels-MacBook-Pro.local>
2024-05-09 19:55:50 -0600

    route complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root "welcome#index"
   get '/register', to: 'users#new', as: 'register_user'
 
-  resources :users, only: [:show, :create] do
-    resources :discover, only: [:index]
-    resources :movies, only: [:index, :show]
-    resources :viewing_parties, only: [:new]
+    resources :users, only: [:show, :create] do
+      resources :discover, only: [:index]
+      resources :movies, only: [:index, :show] do
+        resources :viewing_parties, only: [:new], path: 'viewing_party'
+    end
   end
 end
-# /users/:user_id/movies/:movie_id/viewing_party/new
